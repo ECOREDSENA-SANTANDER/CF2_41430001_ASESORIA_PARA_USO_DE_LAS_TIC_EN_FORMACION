b99aadef9d8dce7de3ea17293c726861c14351f3
PABG1990 <pedro19908454@gmail.com>
2026-04-28 13:40:02 -0500

    Act

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -27,8 +27,8 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-        figcaption Video. 
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/0RqHDve7Gpw?si=_wp2yKVLls2tQmVC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        figcaption Video. Ejecución y evaluación del plan de formación
 
 
 </template>
